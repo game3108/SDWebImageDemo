60c94048486d505171002ed374b11abf878c3fc0
game3108 <game3108@qq.com>
2016-10-09 18:27:14 +0800

    add document

--- SDWebImage-3.8.2/SDWebImage/SDWebImageCompat.m
+++ SDWebImage-3.8.2/SDWebImage/SDWebImageCompat.m
@@ -16,17 +16,18 @@ inline UIImage *SDScaledImageForKey(NSString *key, UIImage *image) {
     if (!image) {
         return nil;
     }
-    
+    //如果多张图片说明是gif图片
     if ([image.images count] > 0) {
         NSMutableArray *scaledImages = [NSMutableArray array];
 
         for (UIImage *tempImage in image.images) {
             [scaledImages addObject:SDScaledImageForKey(key, tempImage)];
         }
-
+        //创建gif图片对象
         return [UIImage animatedImageWithImages:scaledImages duration:image.duration];
     }
     else {
+        //屏幕包含缩放尺寸
         if ([[UIScreen mainScreen] respondsToSelector:@selector(scale)]) {
             CGFloat scale = 1;
             if (key.length >= 8) {
@@ -40,7 +41,7 @@ inline UIImage *SDScaledImageForKey(NSString *key, UIImage *image) {
                     scale = 3.0;
                 }
             }
-
+            //图片进行缩放适应屏幕
             UIImage *scaledImage = [[UIImage alloc] initWithCGImage:image.CGImage scale:scale orientation:image.imageOrientation];
             image = scaledImage;
         }
